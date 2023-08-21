c8b2a1a3e12b71b46e4fc2a2d6bfa112fb68165a
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2023-08-21 10:31:59 -0700

    reformat

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,6 +1,6 @@
-use criterion::{ criterion_group, criterion_main };
 use criterion::black_box;
 use criterion::Criterion;
+use criterion::{criterion_group, criterion_main};
 
 fn is_none(option: Option<i32>) {
     black_box(option.is_none());
@@ -11,14 +11,8 @@ fn eq_none(option: Option<i32>) {
 }
 
 pub fn benchmark(criterion: &mut Criterion) {
-    criterion.bench_function(
-        "is none",
-        |b| b.iter(|| is_none(black_box(Some(5))))
-    );
-    criterion.bench_function(
-        "eq none",
-        |b| b.iter(|| eq_none(black_box(Some(5))))
-    );
+    criterion.bench_function("is none", |b| b.iter(|| is_none(black_box(Some(5)))));
+    criterion.bench_function("eq none", |b| b.iter(|| eq_none(black_box(Some(5)))));
 }
 
 criterion_group!(benches, benchmark);
