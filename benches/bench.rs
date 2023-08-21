d70be2de124e007cd9fd850c68fda795151eeb5c
MrEconomical <47700125+MrEconomical@users.noreply.github.com>
2023-08-21 12:02:06 -0700

    add matches case

--- benches/bench.rs
+++ benches/bench.rs
@@ -10,9 +10,23 @@ fn eq_none(option: Option<i32>) {
     black_box(option == None);
 }
 
+fn matches_none(option: Option<i32>) {
+    black_box(matches!(option, None));
+}
+
 pub fn benchmark(criterion: &mut Criterion) {
-    criterion.bench_function("is none", |b| b.iter(|| is_none(black_box(Some(5)))));
-    criterion.bench_function("eq none", |b| b.iter(|| eq_none(black_box(Some(5)))));
+    criterion.bench_function("is none/some", |b| b.iter(|| is_none(black_box(Some(5)))));
+    criterion.bench_function("is none/none", |b| b.iter(|| is_none(black_box(None))));
+
+    criterion.bench_function("eq none/some", |b| b.iter(|| eq_none(black_box(Some(5)))));
+    criterion.bench_function("eq none/none", |b| b.iter(|| eq_none(black_box(None))));
+
+    criterion.bench_function("matches none/some", |b| {
+        b.iter(|| matches_none(black_box(Some(5))))
+    });
+    criterion.bench_function("matches none/none", |b| {
+        b.iter(|| matches_none(black_box(None)))
+    });
 }
 
 criterion_group!(benches, benchmark);
